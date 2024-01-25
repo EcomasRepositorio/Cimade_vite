ac6ec887e3361466975f7ac1ed29c01ce2b58a8c
jhoosy <maycolmedina366@gmail.com>
2024-01-24 23:14:41 -0500

    quite la logica de las animaciones.json y agregue img.webp

--- components/homeContend/Counter.jsx
+++ components/homeContend/Counter.jsx
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ItemCard } from "./ItemCard";
-import animationEgresados from '/ecomas/cimade/Cimade_vite/public/icons/avatar.json'; 
-import animationAlumnos from '/ecomas/cimade/Cimade_vite/public/icons/alumnos.json'; 
-import animationDiplomados from '/ecomas/cimade/Cimade_vite/public/icons/diplomados.json';
-import animationCursos from '/ecomas/cimade/Cimade_vite/public/icons/cursos.json';
 
-{/* LOGICA DE , INICIAR CUANDO SE VEA EL COMPONENTE*/}
 const Counter = () => {
   const [counterAnimated, setCounterAnimated] = useState(false);
 
@@ -26,16 +21,14 @@ const Counter = () => {
     };
   }, [counterAnimated]);
 
-
-  {/*ICONOS DEL CONTADOR*/}
   return (
     <div id="counter-section">
       <div className="mx-auto container md:-mt-28 -mt-20 flex justify-center items-center">
         <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-2 gap-x-2 gap-y-2 lg:gap-x-6 md:gap-x-6 md:gap-y-6">
-          <ItemCard  number={"2597+"} description={"Egresados"} animated={counterAnimated} animation="fade-in-left" />
-          <ItemCard number={"6983+"} description={"Alumnos"} animated={counterAnimated} animation="fade-in-right" />
-          <ItemCard number={"7"} description={"Diplomados"} animated={counterAnimated} animation="fade-in-up" />
-          <ItemCard number={"18"} description={"Cursos"} animated={counterAnimated} animation="" />
+          <ItemCard number={"2597+"} description={"Egresados"} animated={counterAnimated} icon="/icons/icono_egresados.webp" />
+          <ItemCard number={"6983+"} description={"Alumnos"} animated={counterAnimated} icon="/icons/icono_alumnos.webp" />
+          <ItemCard number={"7"} description={"Diplomados"} animated={counterAnimated} icon="/icons/icono_diplomado.webp" />
+          <ItemCard number={"18"} description={"Cursos"} animated={counterAnimated} icon="/icons/icono_curso.webp" />
         </div>
       </div>
     </div>
--- components/homeContend/ItemCard.jsx
+++ components/homeContend/ItemCard.jsx
@@ -1,50 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import Lottie from 'react-lottie';
-import animationEgresados from '/ecomas/cimade/Cimade_vite/public/icons/avatar.json'; 
-import animationAlumnos from '/ecomas/cimade/Cimade_vite/public/icons/alumnos.json'; 
-import animationDiplomados from '/ecomas/cimade/Cimade_vite/public/icons/diplomados.json';
-import animationCursos from '/ecomas/cimade/Cimade_vite/public/icons/cursos.json';
 
-export const ItemCard = ({ number, description, animated, animation }) => {
+export const ItemCard = ({ number, description, animated, animation, icon }) => {
   const [count, setCount] = useState(0);
 
-  {/* ICONOS DEL CONTADOR */}
-  const lottieOptions = {
-    loop: false,
-    autoplay: false,
-    animationData: animation === 'fade-in-left'
-      ? animationEgresados
-      : animation === 'fade-in-right'
-        ? animationAlumnos
-        : animation === 'fade-in-up'
-          ? animationDiplomados
-          : animationCursos,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
-
-
-  {/* LOGICA DEL CONTADOR  */}
   useEffect(() => {
     let interval;
     if (animated) {
       interval = setInterval(() => {
         setCount((prevCount) => (prevCount < parseInt(number, 10) ? prevCount + 100 : parseInt(number, 10)));
-      }, 25); // Ajusta la velocidad del contador según tus preferencias
+      }, 25);
     }
     return () => clearInterval(interval);
   }, [animated, number]);
 
   return (
     <div className={`flex justify-center flex-col items-center w-36 h-36 md:w-44 md:h-48 lg:w-56 lg:h-56 bg-white shadow-md rounded-2xl ${animated ? `animate-${animation}` : ''}`}>
-      {animated && <Lottie options={lottieOptions} height={120} width={120} speed={0.5} />}
+      {icon && <img src={icon} alt="Icono" className="w-10 h-10 md:w-16 md:h-16 lg:w-20 lg:h-20 mb-4" />}
       <h3 className="lg:text-5xl md:text-4xl text-2xl font-extrabold leading-10 text-center text-gray-800">
         {count}
       </h3>
-      <p className="mt-4 text-sm md:text-base lg:text-lg leading-none text-center text-gray-600 font-bold ">
+      <p className="mt-4 text-sm md:text-base lg:text-lg leading-none text-center text-gray-600 font-bold">
         {description}
       </p>
     </div>
   );
 };
+
+export default ItemCard;
