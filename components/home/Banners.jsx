5c92825b4537432b27bd8229b63905387de88d52
Jhosy <maycolmedina366@gmail.com>
2024-02-01 18:12:00 -0500

    agregue mas contenido a mi banners

--- components/home/Banners.jsx
+++ components/home/Banners.jsx
@@ -50,65 +50,18 @@ const Banners = () => {
 
   return (
     <>
+      <div class="bannerBack">
       <header>
-    <div class="logo">JhosyDev</div>
-    <nav>
-        <a href="">Home</a>
-        <a href="">Info</a>
-        <a href="">Contact</a>
-    </nav>
+   
     
 </header>
     <div class="carousel">
         <div class="list">
-            <div class="item">
-                <img src="images/img1.png"/>
-                <div class="introduce">
-                    <div class="title">The best Dj all time</div>
-                    <div class="topic">AVICII</div>
-                    <div class="des">
-                      
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.
-                    </div>
-                    <button class="seeMore">SEE MORE &#8599</button>
-                </div>
-                <div class="detail">
-                    <div class="title">Aerphone GHTK</div>
-                    <div class="des">
-              
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, reiciendis suscipit nobis nulla animi, modi explicabo quod corrupti impedit illo, accusantium in eaque nam quia adipisci aut distinctio porro eligendi. Reprehenderit nostrum consequuntur ea! Accusamus architecto dolores modi ducimus facilis quas voluptatibus! Tempora ratione accusantium magnam nulla tenetur autem beatae.
-                    </div>
-                    <div class="specifications">
-                        <div>
-                            <p>Used Time</p>
-                            <p>6 hours</p>
-                        </div>
-                        <div>
-                            <p>Charging port</p>
-                            <p>Type-C</p>
-                        </div>
-                        <div>
-                            <p>Compatible</p>
-                            <p>Android</p>
-                        </div>
-                        <div>
-                            <p>Bluetooth</p>
-                            <p>5.3</p>
-                        </div>
-                        <div>
-                            <p>Controlled</p>
-                            <p>Touch</p>
-                        </div>
-                    </div>
-                    <div class="checkout">
-                        <button>ADD TO CART</button>
-                        <button>CHECKOUT</button>
-                    </div>
-                </div>
-            </div>
 
-            <div class="item">
-                <img src="images/img2.png"/>
+
+
+        <div class="item">
+                <img src="image/traversoTree.png"/>
                 <div class="introduce">
                     <div class="title">ES</div>
                     <div class="topic">ES</div>
@@ -153,13 +106,66 @@ const Banners = () => {
                 </div>
             </div>
 
+
+
+
+
+
+
             <div class="item">
-                <img src="images/img3.png"/>
+                <img src="image/traversoTwo.png"/>
                 <div class="introduce">
-                    <div class="title">EL</div>
-                    <div class="topic">EL</div>
+                    <div class="title">CIMADE</div>
+                    <div class="topic">AVICII </div>
                     <div class="des">
-                   
+                  
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.
+                    </div>
+                    <button class="seeMore">SEE MOREss &#8599</button>
+                </div>
+                <div class="detail">
+                    <div class="title">Aerphone GHTK</div>
+                    <div class="des">
+                  
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, reiciendis suscipit nobis nulla animi, modi explicabo quod corrupti impedit illo, accusantium in eaque nam quia adipisci aut distinctio porro eligendi. Reprehenderit nostrum consequuntur ea! Accusamus architecto dolores modi ducimus facilis quas voluptatibus! Tempora ratione accusantium magnam nulla tenetur autem beatae.
+                    </div>
+                    <div class="specifications">
+                        <div>
+                            <p>Used Time</p>
+                            <p>6 hours</p>
+                        </div>
+                        <div>
+                            <p>Charging port</p>
+                            <p>Type-C</p>
+                        </div>
+                        <div>
+                            <p>Compatible</p>
+                            <p>Android</p>
+                        </div>
+                        <div>
+                            <p>Bluetooth</p>
+                            <p>5.3</p>
+                        </div>
+                        <div>
+                            <p>Controlled</p>
+                            <p>Touch</p>
+                        </div>
+                    </div>
+                    <div class="checkout">
+                        <button>ADD TO CART</button>
+                        <button>CHECKOUT</button>
+                    </div>
+                </div>
+            </div>
+           
+
+            <div class="item">
+                <img src="image/traversoOne.png"/>
+                <div class="introduce">
+                    <div class="title">ES</div>
+                    <div class="topic">ES</div>
+                    <div class="des">
+                  
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.
                     </div>
                     <button class="seeMore">SEE MORE &#8599</button>
@@ -167,7 +173,7 @@ const Banners = () => {
                 <div class="detail">
                     <div class="title">Aerphone GHTK</div>
                     <div class="des">
-                     
+                  
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, reiciendis suscipit nobis nulla animi, modi explicabo quod corrupti impedit illo, accusantium in eaque nam quia adipisci aut distinctio porro eligendi. Reprehenderit nostrum consequuntur ea! Accusamus architecto dolores modi ducimus facilis quas voluptatibus! Tempora ratione accusantium magnam nulla tenetur autem beatae.
                     </div>
                     <div class="specifications">
@@ -199,143 +205,7 @@ const Banners = () => {
                 </div>
             </div>
 
-            <div class="item">
-                <img src="images/img4.png"/>
-                <div class="introduce">
-                    <div class="title">MEJOR</div>
-                    <div class="topic">MEJOR</div>
-                    <div class="des">
-                       
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.
-                    </div>
-                    <button class="seeMore">SEE MORE &#8599</button>
-                </div>
-                <div class="detail">
-                    <div class="title">Aerphone GHTK</div>
-                    <div class="des">
-                
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, reiciendis suscipit nobis nulla animi, modi explicabo quod corrupti impedit illo, accusantium in eaque nam quia adipisci aut distinctio porro eligendi. Reprehenderit nostrum consequuntur ea! Accusamus architecto dolores modi ducimus facilis quas voluptatibus! Tempora ratione accusantium magnam nulla tenetur autem beatae.
-                    </div>
-                    <div class="specifications">
-                        <div>
-                            <p>Used Time</p>
-                            <p>6 hours</p>
-                        </div>
-                        <div>
-                            <p>Charging port</p>
-                            <p>Type-C</p>
-                        </div>
-                        <div>
-                            <p>Compatible</p>
-                            <p>Android</p>
-                        </div>
-                        <div>
-                            <p>Bluetooth</p>
-                            <p>5.3</p>
-                        </div>
-                        <div>
-                            <p>Controlled</p>
-                            <p>Touch</p>
-                        </div>
-                    </div>
-                    <div class="checkout">
-                        <button>ADD TO CART</button>
-                        <button>CHECKOUT</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="item">
-                <img src="images/img5.png"/>
-                <div class="introduce">
-                    <div class="title">DE </div>
-                    <div class="topic">DE</div>
-                    <div class="des">
-                     
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.
-                    </div>
-                    <button class="seeMore">SEE MORE &#8599</button>
-                </div>
-                <div class="detail">
-                    <div class="title">Aerphone GHTK</div>
-                    <div class="des">
-                    
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, reiciendis suscipit nobis nulla animi, modi explicabo quod corrupti impedit illo, accusantium in eaque nam quia adipisci aut distinctio porro eligendi. Reprehenderit nostrum consequuntur ea! Accusamus architecto dolores modi ducimus facilis quas voluptatibus! Tempora ratione accusantium magnam nulla tenetur autem beatae.
-                    </div>
-                    <div class="specifications">
-                        <div>
-                            <p>Used Time</p>
-                            <p>6 hours</p>
-                        </div>
-                        <div>
-                            <p>Charging port</p>
-                            <p>Type-C</p>
-                        </div>
-                        <div>
-                            <p>Compatible</p>
-                            <p>Android</p>
-                        </div>
-                        <div>
-                            <p>Bluetooth</p>
-                            <p>5.3</p>
-                        </div>
-                        <div>
-                            <p>Controlled</p>
-                            <p>Touch</p>
-                        </div>
-                    </div>
-                    <div class="checkout">
-                        <button>ADD TO CART</button>
-                        <button>CHECKOUT</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="item">
-                <img src="images/img6.png"/>
-                <div class="introduce">
-                    <div class="title">TODOS!</div>
-                    <div class="topic">TODOS!</div>
-                    <div class="des">
-               
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, laborum cumque dignissimos quidem atque et eligendi aperiam voluptates beatae maxime.
-                    </div>
-                    <button class="seeMore">SEE MORE &#8599</button>
-                </div>
-                <div class="detail">
-                    <div class="title">Aerphone GHTK</div>
-                    <div class="des">
-                     
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, reiciendis suscipit nobis nulla animi, modi explicabo quod corrupti impedit illo, accusantium in eaque nam quia adipisci aut distinctio porro eligendi. Reprehenderit nostrum consequuntur ea! Accusamus architecto dolores modi ducimus facilis quas voluptatibus! Tempora ratione accusantium magnam nulla tenetur autem beatae.
-                    </div>
-                    <div class="specifications">
-                        <div>
-                            <p>Used Time</p>
-                            <p>6 hours</p>
-                        </div>
-                        <div>
-                            <p>Charging port</p>
-                            <p>Type-C</p>
-                        </div>
-                        <div>
-                            <p>Compatible</p>
-                            <p>Android</p>
-                        </div>
-                        <div>
-                            <p>Bluetooth</p>
-                            <p>5.3</p>
-                        </div>
-                        <div>
-                            <p>Controlled</p>
-                            <p>Touch</p>
-                        </div>
-                    </div>
-                    <div class="checkout">
-                        <button>ADD TO CART</button>
-                        <button>CHECKOUT</button>
-                    </div>
-                </div>
-            </div>
+            
         </div>
         <div class="arrows">
             <button id="prev">{"<"}</button>
@@ -343,6 +213,7 @@ const Banners = () => {
             <button id="back">See All  &#8599</button>
         </div>
     </div>
+      </div>
     </>
   );
 };
