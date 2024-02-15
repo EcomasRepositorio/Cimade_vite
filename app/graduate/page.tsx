8c78a6ad7ff4cfccbe43ebe23c6aac42f6507da7
Jhosy <maycolmedina366@gmail.com>
2024-02-15 16:35:08 -0500

    cree los arrays , los cuales contendran todos los cursos

--- app/graduate/page.tsx
+++ app/graduate/page.tsx
@@ -1,24 +1,26 @@
 "use client";
+import Link from "next/link";
 import React, { useState, useEffect } from "react";
+import { lista1 } from "@/components/utils/courses";
+import { lista2 } from "@/components/utils/courses";
+import { lista3 } from "@/components/utils/courses";
+import { lista4 } from "@/components/utils/courses";
+import { lista5 } from "@/components/utils/courses";
 
-const page = () => {
+const Page = () => {
   const [isAccordionOpen, setIsAccordionOpen] = useState(false);
- 
 
-  useEffect(() => {
-    // Any client-side logic can be placed here
-    // This effect runs only on the client side
-  }, []); // Empty dependency array ensures it runs only once on mount
+  useEffect(() => {}, []);
 
   const toggleAccordion = () => {
     setIsAccordionOpen(!isAccordionOpen);
   };
 
   return (
-    <div id="/graduate" >
+    <div id="/graduate">
       <div className=" p-5 flex flex-col ">
-        <div className="text text-center text-4xl md:text-5xl lg:text-6xl xl:text-7xl mb-4 font-bold text-primaryblue " >
-          DIPLOMADOS 2024
+        <div className="text text-center text-4xl md:text-5xl lg:text-4xl xl:text-7xl mb-4 font-bold font-poppins text-black">
+          DIPLOMADOS - INGENIERIA CIVIL
         </div>
         <div className="py-12 md:px-20 sm:px-14 px-6 flex flex-col sm:flex-row justify-center">
           <div className="sm:w-96 mb-4 sm:mb-0">
@@ -29,37 +31,132 @@ const page = () => {
             />
           </div>
           <div className="sm:w-1/2 md:px-20 sm:px-5">
-            <div className="text-gray-800 font-bold lg:text-6xl text-4xl my-2">
+            <div className="text-black font-bold font-poppins lg:text-5xl text-5xl my-2">
               ASISTENTE TÉCNICO EN OBRAS
             </div>
             <div className="text-gray-700 mb-2 md:mb-6 font-mono text-2xl">
-              <p>Inicio 17 de febrero</p>
-              <p>modalidad online</p>
-              <p>4 meses</p>
-              <p>192 horas académicas</p>
+              <div className="flex items-center">
+                <img src="/icons/calendar.png" alt="" className="mr-2" />
+                <p className="font-semibold">Inicio 17 de febrero</p>
+              </div>
+
+              <div className="flex items-center">
+                <img src="/icons/check.png" alt="" className="mr-2" />
+                <p className="font-semibold">Modalidad online</p>
+              </div>
+
+              <div className="flex items-center">
+                <img src="/icons/check.png" alt="" className="mr-2" />
+                <p className="font-semibold">4 meses</p>
+              </div>
+
+              <div className="flex items-center">
+                <img src="/icons/check.png" alt="" className="mr-2" />
+                <p className="font-semibold">192 horas académicas</p>
+              </div>
             </div>
-            <div className="flex justify-left mb-2">
-              <button className="border bg-primaryblue flex items-center justify-center sm:inline-block  mb-4 text-white font-bold rounded-full h-26 w-26  ">
+
+            <div></div>
+
+            <div className="flex flex-col items-center">
+              <Link
+                className="border bg-primaryceleste flex items-center justify-center mb-4 text-white font-bold rounded-full h-16 w-full "
+                href="https://api.whatsapp.com/send?phone=51900102090"
+                target="_blank"
+              >
                 Mas información
-              </button>
+              </Link>
               <button
                 onClick={toggleAccordion}
-                className="border bg-primaryblue flex items-center justify-center sm:inline-block  mb-4 text-white font-bold rounded-full h-16 w-36 ml-8"
+                className="border bg-primaryceleste flex items-center justify-center mb-4 text-white font-bold rounded-full h-16 w-full"
               >
                 Cursos
+                <img className=" ml-8" src="/icons/flecha.png" alt="op" />
               </button>
             </div>
             {isAccordionOpen && (
-              <div className="bg-primaryblue">
-                {/* Contenido del acordeón aquí */}
-                <p>Contenido del curso ....</p>
+              <div className="bg-slate-200 rounded-lg">
+                <div className="text-textblue text-left py-4 font-sans font-semibold ml-8">
+                  <ul>
+                    {lista1.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             )}
           </div>
         </div>
+
+        <div className="py-12 md:px-20 sm:px-14 px-6 flex flex-col sm:flex-row justify-center">
+          <div className="sm:w-96 mb-4 sm:mb-0">
+            <img
+              className="bg-cover w-full h-64 sm:h-auto rounded-3xl"
+              src="/image/ing_small.jpg"
+              alt="icon"
+            />
+          </div>
+          <div className="sm:w-1/2 md:px-20 sm:px-5">
+            <div className="text-black font-bold font-poppins lg:text-5xl text-5xl my-2">
+              ASISTENTE TÉCNICO EN OBRAS
+            </div>
+            <div className="text-gray-700 mb-2 md:mb-6 font-mono text-2xl">
+              <div className="flex items-center">
+                <img src="/icons/calendar.png" alt="" className="mr-2" />
+                <p className="font-semibold">Inicio 17 de febrero</p>
+              </div>
+
+              <div className="flex items-center">
+                <img src="/icons/check.png" alt="" className="mr-2" />
+                <p className="font-semibold">Modalidad online</p>
+              </div>
+
+              <div className="flex items-center">
+                <img src="/icons/check.png" alt="" className="mr-2" />
+                <p className="font-semibold">4 meses</p>
+              </div>
+
+              <div className="flex items-center">
+                <img src="/icons/check.png" alt="" className="mr-2" />
+                <p className="font-semibold">192 horas académicas</p>
+              </div>
+            </div>
+
+            <div></div>
+
+            <div className="flex flex-col items-center">
+              <Link
+                className="border bg-primaryceleste flex items-center justify-center mb-4 text-white font-bold rounded-full h-16 w-full "
+                href="https://api.whatsapp.com/send?phone=51900102090"
+                target="_blank"
+              >
+                Mas información
+              </Link>
+              <button
+                onClick={toggleAccordion}
+                className="border bg-primaryceleste flex items-center justify-center mb-4 text-white font-bold rounded-full h-16 w-full"
+              >
+                Cursos
+                <img className=" ml-8" src="/icons/flecha.png" alt="op" />
+              </button>
+            </div>
+            {isAccordionOpen && (
+              <div className="bg-slate-200 rounded-lg">
+                <div className="text-textblue text-left py-4 font-sans font-semibold ml-8">
+                  <ul>
+                    {lista2.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
---
+++ components/utils/courses.tsx
@@ -0,0 +1,79 @@
+
+// Definir 5 listas con constantes de distintos nombres
+export const lista1 = [
+    "1. Expediente técnico en obras",
+    "2. Régimen laboral en construcción civil",
+    "3. Planificación y programación de obras",
+    "4. Control de obras",
+    "5. Metrados en edificaciones",
+    "6. Cuaderno de obra físico y digital",
+    "7. Costos y Presupuestos con S10",
+    "8. Valorización de obras",
+    "9. Liquidación de obras",
+    "10. Formulación y planificación de proyectos con MS Project",
+    "11. Análisis y diseño de instituciones educativas",
+    "12. Seguridad, salud ocupacional y medio ambiente en obras públicas"
+];
+
+// En un archivo llamado listaVial.js
+
+export const lista2 = [
+    "1. Tráfico y carga en proyectos viales",
+    "2. Topografía, diseño geométrico, señalización y seguridad vial",
+    "3. Geología – Geotecnia en obras viales",
+    "4. Suelos y pavimentos en proyectos viales",
+    "5. Hidrología e hidráulica",
+    "6. Estructuras en carreteras – puentes",
+    "7. Metrados, costos y presupuesto – cronograma en obras viales",
+    "8. Arqueología en proyectos viales",
+    "9. Estudios ambientales en proyectos viales",
+    "10. Diseño, construcción y mantenimiento de Túneles",
+    "11. Integración de proyectos de infraestructura vial"
+];
+
+export const lista3 = [
+    "1. Diseño y modelamiento de puentes con CSi BRIDGE",
+    "2. Modelamiento y análisis estructural de puentes",
+    "3. Diseño sísmico en puentes AASHTO-LRFD",
+    "4. Mantenimiento y conservación de puentes",
+    "5. Inspección, evaluación de puentes bajo la metodología SCAP",
+    "6. Diseño de puentes, pontones",
+    "7. Estudios de Hidrología e hidráulica en puentes",
+    "8. Estudios geológicos y geotécnicos en puentes",
+    "9. Diseño y modelamiento de puentes con SAP 2000"
+];
+
+export const lista4 = [
+    "1. Diseño de Sistemas de Concreto para Contención de Tierra y Líquidos",
+    "2. Diseño albañilería confinada",
+    "3. Diseño de Edificios Industriales",
+    "4. Análisis y diseño de instituciones educativas",
+    "5. Diseño de Puentes",
+    "6. Reparación y Refuerzo de Estructuras Existentes",
+    "7. Tópicos Especiales en Proyectos Estructurales",
+    "8. Diseño Sismorresistente en edificio de concreto armado",
+    "9. Diseño de Elementos en Concreto Postensado"
+];
+
+export const lista5 = [
+    "1. Diseño de Sistemas de Concreto para Contención de Tierra y Líquidos",
+    "2. Diseño albañilería confinada",
+    "3. Diseño de Edificios Industriales",
+    "4. Análisis y diseño de instituciones educativas",
+    "5. Diseño de Puentes",
+    "6. Reparación y Refuerzo de Estructuras Existentes",
+    "7. Tópicos Especiales en Proyectos Estructurales",
+    "8. Diseño Sismorresistente en edificio de concreto armado",
+    "9. Diseño de Elementos en Concreto Postensado"
+];
+
+// Crear un objeto para mapear las listas según el nombre
+const listas = {
+  Lista1: lista1,
+  Lista2: lista2,
+  Lista3: lista3,
+  Lista4: lista4,
+  Lista5: lista5
+};
+
+
