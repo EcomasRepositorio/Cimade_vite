1145edadaf98f223059f5bbaeff388e2660c596d
Jhosy <maycolmedina366@gmail.com>
2024-02-20 16:42:00 -0500

    componenete de cursos terminado al 100%

--- app/shipments/page.tsx
+++ app/shipments/page.tsx
@@ -3,7 +3,70 @@ import React from 'react'
 const page = () => {
   return (
     <div  id="/shipments">
-        Jhosy te amo
+       
+       
+       <section className="bg-primaryceleste ">
+       <div className="text-center font-semibold font-poppins">
+          <p style={{ color: "white", padding: "40px", fontSize: "50px" }}>
+           REALIZA EL SEGUIMIENTO A TU CERTIFICADO
+          </p>
+          </div>
+    <div className="container px-6 py-10 mx-auto">
+        <div className="lg:-mx-6 lg:flex lg:items-center">
+            <img className="object-cover object-center lg:w-1/2 lg:mx-6 w-full h-96 rounded-lg lg:h-[36rem]" src="https://images.unsplash.com/photo-1499470932971-a90681ce8530?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/>
+
+            <div className="mt-8 lg:w-1/2 lg:px-6 lg:mt-0">
+                <p className="text-5xl font-semibold text-blue-500 ">“</p>
+
+                <h1 className="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl lg:w-96">
+                    Help us improve our productivity
+                </h1>
+
+                <p className="max-w-lg mt-6 text-gray-500 dark:text-gray-400 ">
+                    “ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad
+                    tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa
+                    aperiam dolorum, obcaecati corrupti aspernatur a. ”
+                </p>
+
+                <h3 className="mt-6 text-lg font-medium text-blue-500">Mia Brown</h3>
+                <p className="text-gray-600 dark:text-gray-300">Marketing Manager at Stech</p>
+                <div className="flex items-center justify-between mt-12 lg:justify-start">
+                <button className='bg-primaryrosa rounded-lg shadow-md py-2 px-4 hover:shadow-lg'>VER SEGUIMIENTO</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>  
+
+<section className="bg-primaryceleste">
+    <div className="container px-6 py-10 mx-auto">
+        <div className="lg:-mx-6 lg:flex lg:items-center">
+            <img className="object-cover object-center lg:w-1/2 lg:mx-6 w-full h-96 rounded-lg lg:h-[36rem]" src="https://images.unsplash.com/photo-1499470932971-a90681ce8530?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/>
+
+            <div className="mt-8 lg:w-1/2 lg:px-6 lg:mt-0">
+                <p className="text-5xl font-semibold text-blue-500 ">“</p>
+
+                <h1 className="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl lg:w-96">
+                    Help us improve our productivity
+                </h1>
+
+                <p className="max-w-lg mt-6 text-gray-500 dark:text-gray-400 ">
+                    “ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad
+                    tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa
+                    aperiam dolorum, obcaecati corrupti aspernatur a. ”
+                </p>
+
+                <h3 className="mt-6 text-lg font-medium text-blue-500">Mia Brown</h3>
+                <p className="text-gray-600 dark:text-gray-300">Marketing Manager at Stech</p>
+
+                <div className="flex items-center justify-between mt-12 lg:justify-start">
+                   <button className='bg-primaryrosa rounded-lg shadow-md py-2 px-4 hover:shadow-lg'>VER SEGUIMIENTO</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
     </div>
   )
 }
--- components/homeContend/Courses.jsx
+++ components/homeContend/Courses.jsx
@@ -19,10 +19,10 @@ const Courses = () => {
         <section className="bg-gray-2 dark:bg-dark pt-20 pb-10 lg:pt-[120px] lg:pb-20">
           <div className="container mx-auto">
             <div className="flex flex-wrap -mx-4">
-              <div className="w-full px-4 md:w-1/2 xl:w-1/3 transform hover:-translate-y-1 transition duration-600">
+              <div className="w-full px-4 md:w-1/2 xl:w-1/3 h-full transform hover:-translate-y-1 transition duration-600">
                 <div className="mb-10 overflow-hidden duration-300 bg-white rounded-2xl dark:bg-dark-2 shadow-1 hover:shadow-3  dark:shadow-card dark:hover:shadow-3">
                   <img
-                    src="/image/ing_riego.jpg"
+                    src="/image/topografia.png"
                     alt="image"
                     className="w-full"
                   />
@@ -32,18 +32,55 @@ const Courses = () => {
                         href="javascript:void(0)"
                         className="text-dark dark:text-textblue hover:text-primaryceleste mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
                       >
-                        50+ Best creative website themes & templates
+                        Topografía, Diseño Geométrico, Señalización y Seguridad Vial
                       </a>
                     </h3>
                     <p className="text-base leading-relaxed text-body-color dark:text-dark-6 mb-7">
-                      Lorem ipsum dolor sit amet pretium consectetur adipiscing
-                      elit. Lorem consectetur adipiscing elit.
+                    Curso integral de topografía, diseño geométrico, señalización y seguridad vial que abarca de manera exhaustiva los fundamentos esenciales para la planificación, diseño y mantenimiento eficiente y seguro de infraestructuras viales. Este programa proporciona a los participantes una sólida base de conocimientos. 
                     </p>
                     <a
-                      href="javascript:void(0)"
+                      href={`https://wa.me/51900102090/?text=${encodeURIComponent(
+                       
+                      )}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="inline-block py-2 text-base font-medium transition border-primaryrosa border rounded-full text-primaryblue hover:border-primaryrosa hover:bg-primaryrosa border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6"
                     >
-                      View Details
+                      Mas Información
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-full px-4 md:w-1/2 xl:w-1/3 h-full transform hover:-translate-y-1 transition duration-600">
+                <div className="mb-10 overflow-hidden duration-300 bg-white rounded-2xl dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3">
+                  <img
+                    src="/image/sistema_riego.png"
+                    alt="image"
+                    className="w-full"
+                  />
+                  <div className="p-8 text-center sm:p-9 md:p-7 xl:p-9">
+                    <h3>
+                      <a
+                        href="javascript:void(0)"
+                        className="text-dark dark:text-textblue hover:text-primaryceleste mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
+                      >
+                        Sistemas de Riego Tecnificado por Goteo
+                      </a>
+                    </h3>
+                    <p className="text-base leading-relaxed text-body-color dark:text-dark-6 mb-7">
+                     Sistema de Riego por Goteo: Curso sobre implementación y gestión de sistemas agrícolas de riego con enfoque en la técnica de goteo. Los participantes adquirirán conocimientos prácticos en diseño, instalación y mantenimiento, optimizando recursos y mejorando la productividad agrícola en cualquier sector.
+
+                    </p>
+                    <a
+                      href={`https://wa.me/51900102090/?text=${encodeURIComponent(
+                       
+                      )}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="inline-block py-2 text-base font-medium transition border-primaryrosa border rounded-full text-primaryblue hover:border-primaryrosa hover:bg-primaryrosa border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6"
+                    >
+                      Mas Información
                     </a>
                   </div>
                 </div>
@@ -52,7 +89,7 @@ const Courses = () => {
               <div className="w-full px-4 md:w-1/2 xl:w-1/3 transform hover:-translate-y-1 transition duration-600">
                 <div className="mb-10 overflow-hidden duration-300 bg-white rounded-2xl dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3">
                   <img
-                    src="/image/ing_riego.jpg"
+                    src="/image/valorizacion_residuos.png"
                     alt="image"
                     className="w-full"
                   />
@@ -62,18 +99,21 @@ const Courses = () => {
                         href="javascript:void(0)"
                         className="text-dark dark:text-textblue hover:text-primaryceleste mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
                       >
-                        50+ Best creative website themes & templates
+                       Valorización de Residuos Sólidos
                       </a>
                     </h3>
                     <p className="text-base leading-relaxed text-body-color dark:text-dark-6 mb-7">
-                      Lorem ipsum dolor sit amet pretium consectetur adipiscing
-                      elit. Lorem consectetur adipiscing elit.
+                    Curso sobre Valorización de Residuos Sólidos: Explora estrategias sostenibles para gestionar residuos sólidos. Se enseñan métodos eficientes de identificación, clasificación y aprovechamiento, promoviendo reciclaje, reutilización y conversión en recursos para reducir impactos ambientales y fomentar la economía circular y mejorar de manera significativa nuestro medio ambiente.
                     </p>
                     <a
-                      href="javascript:void(0)"
+                      href={`https://wa.me/51900102090/?text=${encodeURIComponent(
+                       
+                      )}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="inline-block py-2 text-base font-medium transition border-primaryrosa border rounded-full text-primaryblue hover:border-primaryrosa hover:bg-primaryrosa border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6"
                     >
-                      View Details
+                      Mas Información
                     </a>
                   </div>
                 </div>
@@ -82,7 +122,7 @@ const Courses = () => {
               <div className="w-full px-4 md:w-1/2 xl:w-1/3 transform hover:-translate-y-1 transition duration-600">
                 <div className="mb-10 overflow-hidden duration-300 bg-white rounded-2xl dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3">
                   <img
-                    src="/image/ing_alimentarias.jpg"
+                    src="/image/calidad_alimentaria.png"
                     alt="image"
                     className="w-full"
                   />
@@ -92,18 +132,22 @@ const Courses = () => {
                         href="javascript:void(0)"
                         className="text-dark dark:text-textblue hover:text-primaryceleste mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
                       >
-                        50+ Best creative website themes & templates
+                       Calidad e Inocuidad Alimentaria
                       </a>
                     </h3>
                     <p className="text-base leading-relaxed text-body-color dark:text-dark-6 mb-7">
-                      Lorem ipsum dolor sit amet pretium consectetur adipiscing
-                      elit. Lorem consectetur adipiscing elit.
+                    Curso sobre Calidad e Inocuidad Alimentaria: Explora los principios y prácticas esenciales para garantizar la seguridad y calidad de los alimentos desde su producción hasta el consumo. Este programa aborda estándares internacionales,
+                     controles de calidad, buenas prácticas de manufactura y sistemas de gestión que aseguran la inocuidad alimentaria, cumpliendo con regulaciones y contribuyendo a la protección de la salud pública.
                     </p>
                     <a
-                      href="javascript:void(0)"
+                      href={`https://wa.me/51900102090/?text=${encodeURIComponent(
+                       
+                      )}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="inline-block py-2 text-base font-medium transition border-primaryrosa border rounded-full text-primaryblue hover:border-primaryrosa hover:bg-primaryrosa border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6"
                     >
-                      View Details
+                      Mas Información
                     </a>
                   </div>
                 </div>
@@ -112,7 +156,7 @@ const Courses = () => {
               <div className="w-full px-4 md:w-1/2 xl:w-1/3 transform hover:-translate-y-1 transition duration-600">
                 <div className="mb-10 overflow-hidden duration-300 bg-white rounded-2xl dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3">
                   <img
-                    src="/image/ing_alimentarias.jpg"
+                    src="/image/elaboracion_matriz.png"
                     alt="image"
                     className="w-full"
                   />
@@ -122,18 +166,22 @@ const Courses = () => {
                         href="javascript:void(0)"
                         className="text-dark dark:text-textblue hover:text-primaryceleste mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
                       >
-                        50+ Best creative website themes & templates
+                        Elaboración de Matriz IPERC - Mapa de Riesgos
                       </a>
                     </h3>
                     <p className="text-base leading-relaxed text-body-color dark:text-dark-6 mb-7">
-                      Lorem ipsum dolor sit amet pretium consectetur adipiscing
-                      elit. Lorem consectetur adipiscing elit.
+                    Curso de Elaboración de Matriz IPERC y Mapa de Riesgos: Enfocado en capacitar a los participantes en la identificación, evaluación y gestión de riesgos laborales mediante la elaboración de la Matriz IPERC
+                     y la creación de Mapas de Riesgos. Este programa proporciona herramientas prácticas para fortalecer la seguridad en el entorno laboral y cumplir con los estándares de prevención.
                     </p>
                     <a
-                      href="javascript:void(0)"
+                      href={`https://wa.me/51900102090/?text=${encodeURIComponent(
+                       
+                      )}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="inline-block py-2 text-base font-medium transition border-primaryrosa border rounded-full text-primaryblue hover:border-primaryrosa hover:bg-primaryrosa border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6"
                     >
-                      View Details
+                      Mas Información
                     </a>
                   </div>
                 </div>
@@ -142,7 +190,7 @@ const Courses = () => {
               <div className="w-full px-4 md:w-1/2 xl:w-1/3 transform hover:-translate-y-1 transition duration-600">
                 <div className="mb-10 overflow-hidden duration-300 bg-white rounded-2xl dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3">
                   <img
-                    src="/image/ing_alimentarias.jpg"
+                    src="/image/monitoreo_ocupacionales.png"
                     alt="image"
                     className="w-full"
                   />
@@ -152,49 +200,25 @@ const Courses = () => {
                         href="javascript:void(0)"
                         className="text-dark dark:text-textblue hover:text-primaryceleste mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
                       >
-                        50+ Best creative website themes & templates
+                        Monitoreo de Agentes Ocupacionales
                       </a>
                     </h3>
                     <p className="text-base leading-relaxed text-body-color dark:text-dark-6 mb-7">
-                      Lorem ipsum dolor sit amet pretium consectetur adipiscing
-                      elit. Lorem consectetur adipiscing elit.
+                    Curso de Monitoreo de Agentes Ocupacionales: Brinda conocimientos especializados en técnicas y estrategias para llevar a cabo el monitoreo efectivo de agentes ocupacionales en diversos entornos laborales. Los participantes aprenderán a identificar, evaluar y controlar los riesgos relacionados con sustancias químicas,
+                     físicas o biológicas, contribuyendo así a crear ambientes laborales seguros y saludables para sus trabajadores.
                     </p>
                     <a
-                      href="javascript:void(0)"
+                      href={`https://wa.me/51900102090/?text=${encodeURIComponent(
+                       
+                      )}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="inline-block py-2 text-base font-medium transition border-primaryrosa border rounded-full text-primaryblue hover:border-primaryrosa hover:bg-primaryrosa border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6"
                     >
-                      View Details
+                      Mas Información
                     </a>
-                  </div>
-                </div>
-              </div>
 
-              <div className="w-full px-4 md:w-1/2 xl:w-1/3 transform hover:-translate-y-1 transition duration-600">
-                <div className="mb-10 overflow-hidden duration-300 bg-white rounded-2xl dark:bg-dark-2 shadow-1 hover:shadow-3 dark:shadow-card dark:hover:shadow-3">
-                  <img
-                    src="/image/ing_alimentarias.jpg"
-                    alt="image"
-                    className="w-full"
-                  />
-                  <div className="p-8 text-center sm:p-9 md:p-7 xl:p-9">
-                    <h3>
-                      <a
-                        href="javascript:void(0)"
-                        className="text-dark dark:text-textblue hover:text-primaryceleste mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
-                      >
-                        50+ Best creative website themes & templates
-                      </a>
-                    </h3>
-                    <p className="text-base leading-relaxed text-body-color dark:text-dark-6 mb-7">
-                      Lorem ipsum dolor sit amet pretium consectetur adipiscing
-                      elit. Lorem consectetur adipiscing elit.
-                    </p>
-                    <a
-                      href="javascript:void(0)"
-                      className="inline-block py-2 text-base font-medium transition border-primaryrosa border rounded-full text-primaryblue hover:border-primaryrosa hover:bg-primaryrosa border-gray-3 px-7 hover:text-white dark:border-dark-3 dark:text-dark-6"
-                    >
-                      View Details
-                    </a>
+                  
                   </div>
                 </div>
               </div>
